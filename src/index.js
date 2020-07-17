6a0fc738d77640249a149504a5bafc934fc04527
Shim2k <dev@shimis-mbp.mynet>
2020-07-17 20:10:50 +0300

    added cleanup functiton option

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-const useNamedEffect = (dependecies = [], evaluator) => {
+const useNamedEffect = (dependecies = [], evaluator, cleanup) => {
   const namedEffect = function (cb) {
     try {
       this(() => {
-        if (!evaluator) {
-          cb && cb(...dependecies); return;
+        if (!evaluator && cb) {
+          cb(...dependecies); return;
         }
         if (evaluator && evaluator()) {
           cb(...dependecies);
         }
+
+        if (cleanup) return cleanup
       }, dependecies)
     } catch (e) {
       throw new Error('Make sure to bind useEffect in-order to use namedEffect.');
