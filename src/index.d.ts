570b5a2092670c1e905a99ab39e2df7e475dc04d
Shim2k <shim2k@gmail.com>
2020-07-18 22:58:40 +0300

    added a declaration file

---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+// Type definitions for named-effect
+// Definitions by: Shimi Razilov https://github.com/shim2k
+
+export default function useNamedEffect(dependecies: Array, conditions: function, cleanup: function): function;
